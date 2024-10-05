a1441af42096bfee19c06f8e181511ffbefb8b27
Sivagireeswaran <99676413+Sivagireeswaran@users.noreply.github.com>
2024-10-05 16:24:53 +0530

    Update count char with space.py

--- count char with space.py
+++ count char with space.py
@@ -1,9 +1,17 @@
 a=input()
 b=a.split()
+d=str(b)
 c={}
-for i in range(b):
-  if b[i] in c:
-    c[b[i]] += 1
+  
+for i in range(len(b)):
+  if b[i].isdigit():
+      print("ivalidinput")
+      c={}
+      break
+  elif b[i] in c:
+      c[b[i]] += 1
   else:
-    c[b[i]] = 1
-print(c)
+      c[b[i]] = 1
+for i in c:
+    print(i ,c[i],end = " ")
+
